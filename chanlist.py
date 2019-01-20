cfe4c24d79695e941c6aca3449ae7d947bf85f67
dgw <dgw@technobabbl.es>
2019-01-20 11:53:36 -0600

    Vastly improve code documentation
    Renamed methods to mean something, instead of using test names.
    
    Added a ton of comments to help users use this as a learning example.

--- chanlist.py
+++ chanlist.py
@@ -8,28 +8,45 @@ from sopel.tools import events, Identifier, SopelMemory
 import random
 import re
 
+# This regex was tested against Rizon's LIST output.
+# You might need to play around with it to get sensible
+# output from your favorite network.
 actual_topic = re.compile(r'^\[\+[a-zA-Z]+\] (.*)')
 LOGGER = get_logger(__name__)
 
 
 @rule('.*')
 @event(events.RPL_LISTSTART, events.RPL_LIST, events.RPL_LISTEND)
-def multi_event_handler(bot, trigger):
+def channel_cache_update_handler(bot, trigger):
     if trigger.event == events.RPL_LISTSTART:
+        # Do stuff that should happen before any list items arrive,
+        # such as (in this case) clearing the cache so it can be refilled.
         bot.memory['channel_cache'] = SopelMemory()
         return LOGGER.info("Channel listing started…")
     if trigger.event == events.RPL_LIST:
+        # Every channel in the list will pass through this section.
+        # Make this code efficient, because it might be called thousands of times.
+        # For RPL_LIST, arg 1 is the channel name, arg 2 is the number of users in
+        # that channel, and arg 3 is the topic.
         channel, _, topic = trigger.args[1:]
+        # Rizon prepends the modes to the topic as e.g. [+nt], so the cleanup regex
+        # from above is used to remove that before storing in the cache.
         bot.memory['channel_cache'][channel] = re.sub(actual_topic, r'\1', topic)
         return
     if trigger.event == events.RPL_LISTEND:
+        # If you're listing to a file, for example, you'd close it here.
+        # This sample code just sends a log message (though Sopel won't
+        # output info messages by default).
         return LOGGER.info("Channel listing finished! {} channels' topics stored."
                            .format(len(bot.memory['channel_cache'])))
 
 
 @commands('chanlist')
 @require_admin('Only bot admins may update the channel cache.')
-def test_list(bot, trigger):
+def channel_cache_command(bot, trigger):
+    # bot.write() expects an iterable as input.
+    # Giving it 'LIST' instead of ['LIST'] will make the bot iterate through
+    # the string character-by-character, sending 'L I S T'. Mind this one.
     bot.write(['LIST'])
 
 
